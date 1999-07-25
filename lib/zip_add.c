1a9ac02c4a7337af71585899cfdbe3c6650d6365
Thomas Klausner <wiz@danbala.tuwien.ac.at>
1999-07-25 20:41:57 +0000

    *** empty log message ***
    --HG--
    branch : HEAD

--- lib/zip_add.c
+++ lib/zip_add.c
@@ -6,9 +6,7 @@
 int
 zip_add(struct zip *zf, char *name, zip_read_func *fn, void *state, int comp)
 {
-    int idx;
-    
-    /* XXX: create new entry */
+    _zip_new_entry(zf);
 
-    return zip_replace(zf, idx, name, fn, state, comp);
+    return zip_replace(zf, zf->nentry-1, name, fn, state, comp);
 }
