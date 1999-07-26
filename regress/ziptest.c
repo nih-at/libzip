1928f4ce1b16a4a486342783066228accbf543a9
Thomas Klausner <wiz@danbala.tuwien.ac.at>
1999-07-26 06:36:52 +0000

    *** empty log message ***
    --HG--
    branch : HEAD

--- regress/ziptest.c
+++ regress/ziptest.c
@@ -28,12 +28,13 @@ main(int argc, char *argv[])
 
     seterrinfo(NULL, argv[1]);
     if ((zf=zip_open(argv[1], 0))==NULL) {
-	myerror(ERRZIPSTR, "can't open file: %s", zip_err_str[zip_err]);
+	myerror(ERRZIP, "can't open file: %s", zip_err_str[zip_err]);
 	return 1;
     }
 
+    seterrinfo(NULL, argv[2]);
     if ((destzf=zip_open(argv[2], ZIP_CREATE))==NULL) {
-	myerror(ERRZIPSTR, "can't open file: %s", zip_err_str[zip_err]);
+	myerror(ERRZIP, "can't open file: %s", zip_err_str[zip_err]);
 	return 1;
     }
 
@@ -45,7 +46,7 @@ main(int argc, char *argv[])
 #endif
 
     if (zip_add_zip(destzf, NULL, NULL, zf, 0, 0, 0) == -1)
-	myerror(ERRZIPSTR, "can't add file to zip-file: %s", zip_err_str[zip_err]);
+	myerror(ERRZIP, "can't add file to zip-file: %s", zip_err_str[zip_err]);
 
 #if 0
     zff1= zff_open_index(zf, 1);
@@ -81,12 +82,13 @@ main(int argc, char *argv[])
 #endif
     
     if (zip_close(destzf)!=0) {
-	myerror(ERRZIPSTR, "can't close file: %s", zip_err_str[zip_err]);
+	myerror(ERRZIP, "can't close file: %s", zip_err_str[zip_err]);
 	return 1;
     }
 
+    seterrinfo(NULL, argv[1]);
     if (zip_close(zf)!=0) {
-	myerror(ERRZIPSTR, "can't close file %s", zip_err_str[zip_err]);
+	myerror(ERRZIP, "can't close file %s", zip_err_str[zip_err]);
 	return 1;
     }
     
