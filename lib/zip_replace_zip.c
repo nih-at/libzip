37394e3264e67c2e8692e6cc6a128d4ca81b2176
Dieter Baron <dillo@danbala.tuwien.ac.at>
2004-04-08 16:56:55 +0000

    use flag to copy compressed data via zip_fread
    --HG--
    branch : no-meta

--- lib/zip_replace_zip.c
+++ lib/zip_replace_zip.c
@@ -1,5 +1,5 @@
 /*
-  $NiH: zip_replace_zip.c,v 1.17.4.5 2004/04/06 20:30:06 dillo Exp $
+  $NiH: zip_replace_zip.c,v 1.17.4.6 2004/04/07 12:08:22 dillo Exp $
 
   zip_replace_zip.c -- replace file from zip file
   Copyright (C) 1999, 2003 Dieter Baron and Thomas Klausner
@@ -40,21 +40,12 @@
 #include "zipint.h"
 
 struct read_zip {
-    struct zip *zf;
-    int idx;
-    long fpos;
-    long avail;
-    /* XXX: ... */
-};
-
-struct read_part {
     struct zip_file *zf;
     struct zip_stat st;
     off_t off, len;
 };
 
 static ssize_t read_zip(void *st, void *data, size_t len, enum zip_cmd cmd);
-static ssize_t read_part(void *st, void *data, size_t len, enum zip_cmd cmd);
 
 
 
@@ -76,9 +67,9 @@ int
 _zip_replace_zip(struct zip *zf, int idx, const char *name,
 		 struct zip *srczf, int srcidx, off_t start, off_t len)
 {
-    int ze, se;
-    struct read_zip *z;
-    struct read_part *p;
+    struct zip_error error;
+    struct read_zip *p;
+    int flags;
 
     if (srczf->entry[srcidx].state != ZIP_ST_UNCHANGED
 	&& srczf->entry[srcidx].state != ZIP_ST_RENAMED) {
@@ -87,39 +78,30 @@ _zip_replace_zip(struct zip *zf, int idx, const char *name,
     }
     
     if ((srczf->cdir->entry[srcidx].comp_method != ZIP_CM_STORE
-	 && start == 0 && (len == 0 || len == -1))) {
-	if ((z=(struct read_zip *)malloc(sizeof(struct read_zip))) == NULL) {
-	    _zip_error_set(&zf->error, ZERR_MEMORY, 0);
-	    return -1;
-	}
-	z->zf = srczf;
-	z->idx = srcidx;
-	return _zip_replace(zf, idx, name, read_zip, z, ZIP_CH_ISCOMP);
-    }
-    else {
-	if ((p=malloc(sizeof(*p))) == NULL) {
-	    _zip_error_set(&zf->error, ZERR_MEMORY, 0);
-	    return -1;
-	}
-	
-	ze = srczf->error.zip_err;
-	se = srczf->error.sys_err;
-	
-	if (zip_stat_index(srczf, idx, &p->st) < 0
-	    || (p->zf=zip_fopen_index(srczf, idx)) == NULL) {
-	    free(p);
-	    _zip_error_set(&zf->error,
-			   srczf->error.zip_err, srczf->error.sys_err);
-	    srczf->error.zip_err = ze;
-	    srczf->error.sys_err = se;
+	 && start == 0 && (len == 0 || len == -1)))
+	flags = ZIP_NAME_COMP;
+    else
+	flags = 0;
 
-	    return -1;
-	}
-	p->off = start;
-	p->len = (len ? len : -1);
-
-	return _zip_replace(zf, idx, name, read_part, p, 0);
+    if ((p=malloc(sizeof(*p))) == NULL) {
+	_zip_error_set(&zf->error, ZERR_MEMORY, 0);
+	return -1;
     }
+	
+    _zip_error_copy(&error, &srczf->error);
+	
+    if (zip_stat_index(srczf, idx, &p->st) < 0
+	|| (p->zf=zip_fopen_index(srczf, idx, flags)) == NULL) {
+	free(p);
+	_zip_error_copy(&zf->error, &srczf->error);
+	_zip_error_copy(&srczf->error, &error);
+	
+	return -1;
+    }
+    p->off = start;
+    p->len = (len ? len : -1);
+    
+    return _zip_replace(zf, idx, name, read_zip, p, 0);
 }
 
 
@@ -128,59 +110,16 @@ static ssize_t
 read_zip(void *state, void *data, size_t len, enum zip_cmd cmd)
 {
     struct read_zip *z;
-    int ret;
-    
-    z = (struct read_zip *)state;
-
-    switch (cmd) {
-    case ZIP_CMD_INIT:
-	/* XXX: sets zf->error */
-	if ((z->fpos=_zip_file_get_offset(z->zf, z->idx)) == 0)
-	    return -1;
-	z->avail = z->zf->cdir->entry[z->idx].comp_size;
-	return 0;
-	
-    case ZIP_CMD_READ:
-	if (fseek(z->zf->zp, z->fpos, SEEK_SET) < 0) {
-	    /* XXX: zip_err = ZERR_SEEK; */
-	    return -1;
-	}
-	if ((ret=fread(data, 1, len < z->avail ? len : z->avail,
-		       z->zf->zp)) < 0) {
-	    /* XXX: zip_err = ZERR_READ; */
-	    return -1;
-	}
-	z->fpos += ret;
-	z->avail -= ret;
-	return ret;
-	
-    case ZIP_CMD_CLOSE:
-	free(z);
-	return 0;
-
-    default:
-	;
-    }
-
-    return -1;
-}
-
-
-
-static ssize_t
-read_part(void *state, void *data, size_t len, enum zip_cmd cmd)
-{
-    struct read_part *z;
     char b[8192], *buf;
     int i, n;
 
-    z = (struct read_part *)state;
+    z = (struct read_zip *)state;
     buf = (char *)data;
 
     switch (cmd) {
     case ZIP_CMD_INIT:
 	for (n=0; n<z->off; n+= i) {
-	    i = (z->off-n > 8192 ? 8192 : z->off-n);
+	    i = (z->off-n > sizeof(b) ? sizeof(b) : z->off-n);
 	    if ((i=zip_fread(z->zf, b, i)) < 0) {
 		zip_fclose(z->zf);
 		z->zf = NULL;
