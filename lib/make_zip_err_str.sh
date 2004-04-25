775c25931e6b1b8d5f3b23629ded38860ca9f5bc
Dieter Baron <dillo@danbala.tuwien.ac.at>
2004-04-25 16:20:16 +0000

    add copyright and license blorb. record RCS ID of script and input file in output file
    --HG--
    branch : HEAD

--- lib/make_zip_err_str.sh
+++ lib/make_zip_err_str.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 
+#  $NiH: zip.h,v 1.37 2004/04/16 09:40:26 dillo Exp $
+#
+#  make_zip_err_str.sh: create zip_err_str.c from zip.h
+#  Copyright (C) 1999, 2003, 2004 Dieter Baron and Thomas Klausner
+#
+#  This file is part of libzip, a library to manipulate ZIP archives.
+#  The authors can be contacted at <nih@giga.or.at>
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#  3. The names of the authors may not be used to endorse or promote
+#     products derived from this software without specific prior
+#     written permission.
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
+#  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+#  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+#  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+#  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 if [ "$#" -ne 2 ]
 then
     echo "Usage: $0 in_file out_file" >&2
@@ -13,9 +47,17 @@ then
     exit 1
 fi
 
+rcsid=`echo '$NiH$' | tr -d '$'`
+inrcsid=`sed -n 's/^  \$\([^$]*[^ ]\) *\$$/\1/p' "$1"`
+
 cat <<EOF >> "$2.$$" || exit 1
-/* This file was generated automatically by $0
-   from $1; make changes there. */
+/*
+   This file was generated automatically by $0
+   from $1; make changes there.
+
+	$rcsid
+	$inrcsid
+ */
 
 #include "zip.h"
 #include "zipint.h"
