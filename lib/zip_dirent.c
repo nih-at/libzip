fe3d855cd8aa8b471d8864d846da75ad1fdde487
Dieter Baron <dillo@nih.at>
2010-01-04 20:23:32 +0100

    Pull up changeset 782:f6aed2c1bf60 	Don't call localtime().
    --HG--
    branch : HEAD

--- lib/zip_dirent.c
+++ lib/zip_dirent.c
@@ -473,23 +473,22 @@ _zip_dirent_write(struct zip_dirent *zde, FILE *fp, int localp,
 static time_t
 _zip_d2u_time(int dtime, int ddate)
 {
-    struct tm *tm;
-    time_t now;
+    struct tm tm;
 
-    now = time(NULL);
-    tm = localtime(&now);
-    /* let mktime decide if DST is in effect */
-    tm->tm_isdst = -1;
+    memset(&tm, sizeof(tm), 0);
     
-    tm->tm_year = ((ddate>>9)&127) + 1980 - 1900;
-    tm->tm_mon = ((ddate>>5)&15) - 1;
-    tm->tm_mday = ddate&31;
+    /* let mktime decide if DST is in effect */
+    tm.tm_isdst = -1;
+    
+    tm.tm_year = ((ddate>>9)&127) + 1980 - 1900;
+    tm.tm_mon = ((ddate>>5)&15) - 1;
+    tm.tm_mday = ddate&31;
 
-    tm->tm_hour = (dtime>>11)&31;
-    tm->tm_min = (dtime>>5)&63;
-    tm->tm_sec = (dtime<<1)&62;
+    tm.tm_hour = (dtime>>11)&31;
+    tm.tm_min = (dtime>>5)&63;
+    tm.tm_sec = (dtime<<1)&62;
 
-    return mktime(tm);
+    return mktime(&tm);
 }
 
 
