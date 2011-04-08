a70ca5d794fabe8af929176535440cbf74f4e7ba
Thomas Klausner <tk@giga.or.at>
2011-04-09 00:34:27 +0200

    Start adding UTF-8 support: first step
    Add an internal function that guesses the encoding of a string:
    ASCII, UTF-8, or CP437.
    Add a test program and 3 test cases, one for each encoding.
    
    --HG--
    branch : HEAD

--- lib/zipint.h
+++ lib/zipint.h
@@ -333,6 +333,8 @@ unsigned int _zip_file_get_offset(struct zip *, int);
 
 int _zip_filerange_crc(FILE *, off_t, off_t, uLong *, struct zip_error *);
 
+enum zip_encoding_type _zip_guess_encoding(const zip_uint8_t * const, zip_uint32_t);
+
 struct zip *_zip_open(const char *, FILE *, int, int, int *);
 
 struct zip_source *_zip_source_file_or_p(struct zip *, const char *, FILE *,
