23134fdd1b47e4e01096ae24d535f586a265e0fe
Thomas Klausner <tk@giga.or.at>
2014-03-09 14:02:38 +0100

    Fix shadow warning.

--- lib/zip_dirent.c
+++ lib/zip_dirent.c
@@ -983,11 +983,11 @@ _zip_write8(zip_uint64_t i, FILE *fp)
 
 
 void
-_zip_u2d_time(time_t time, zip_uint16_t *dtime, zip_uint16_t *ddate)
+_zip_u2d_time(time_t intime, zip_uint16_t *dtime, zip_uint16_t *ddate)
 {
     struct tm *tm;
 
-    tm = localtime(&time);
+    tm = localtime(&intime);
     *ddate = (zip_uint16_t)(((tm->tm_year+1900-1980)<<9) + ((tm->tm_mon+1)<<5) + tm->tm_mday);
     *dtime = (zip_uint16_t)(((tm->tm_hour)<<11) + ((tm->tm_min)<<5) + ((tm->tm_sec)>>1));
 
