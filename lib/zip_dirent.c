ef92e112a8bdb04a39569ab616ece6e7358d3d10
Thomas Klausner <tk@giga.or.at>
2015-05-27 15:29:48 +0200

    Truncate to years after 1980 for conversion to DOS dates. Found by clang -fsanitize=undefined.

--- lib/zip_dirent.c
+++ lib/zip_dirent.c
@@ -896,6 +896,10 @@ _zip_u2d_time(time_t intime, zip_uint16_t *dtime, zip_uint16_t *ddate)
     struct tm *tm;
 
     tm = localtime(&intime);
+    if (tm->tm_year < 80) {
+	tm->tm_year = 80;
+    }
+
     *ddate = (zip_uint16_t)(((tm->tm_year+1900-1980)<<9) + ((tm->tm_mon+1)<<5) + tm->tm_mday);
     *dtime = (zip_uint16_t)(((tm->tm_hour)<<11) + ((tm->tm_min)<<5) + ((tm->tm_sec)>>1));
 
