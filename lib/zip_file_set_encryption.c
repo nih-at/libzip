39e11fa5d595861b61cd7b5ceddf354a069bd791
Thomas Klausner <tk@giga.or.at>
2017-02-19 10:59:09 +0100

    Check pointer before dereferencing it.
    Coverity CID-1397271

--- lib/zip_file_set_encryption.c
+++ lib/zip_file_set_encryption.c
@@ -67,7 +67,7 @@ zip_file_set_encryption(zip_t *za, zip_uint64_t idx, zip_uint16_t method, const 
 	    if (e->changes->changed & ZIP_DIRENT_PASSWORD) {
 		_zip_crypto_clear(e->changes->password, strlen(e->changes->password));
 		free(e->changes->password);
-		e->changes->password = e->orig->password;
+		e->changes->password = (e->orig == NULL ? NULL : e->orig->password);
 	    }
 	    e->changes->changed &= ~(ZIP_DIRENT_ENCRYPTION_METHOD|ZIP_DIRENT_PASSWORD);
 	    if (e->changes->changed == 0) {
