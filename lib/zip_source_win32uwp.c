fe596b6292dd7f283fc2c2e2ac6f628753a7478f
Michał Janiszewski <janisozaur@gmail.com>
2017-12-11 00:06:23 +0100

    Move myself to THANKS file

--- lib/zip_source_win32uwp.c
+++ lib/zip_source_win32uwp.c
@@ -1,7 +1,6 @@
 /*
 zip_source_win32uwp.c -- create data source from Windows file (UWP)
-Copyright (C) 1999-2017 Dieter Baron, Thomas Klausner and Michał
-Janiszewski
+Copyright (C) 1999-2017 Dieter Baron and Thomas Klausner.
 
 This file is part of libzip, a library to manipulate ZIP archives.
 The authors can be contacted at <libzip@nih.at>
