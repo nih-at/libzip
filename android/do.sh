4b32194574c4b3ab4074bba8c68f9ba06a6ff20b
Declan <dec1@geoaze.com>
2019-04-22 22:55:52 +0200

    build static and dynamic libs by default

--- android/do.sh
+++ android/do.sh
@@ -9,14 +9,12 @@ rm -rf $INSTALL_DIR
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR #"${ANDROID_TARGET_PLATFORM}"
 
-for ANDROID_TARGET_PLATFORM in armeabi-v7a arm64-v8a x86 x86_64
-do
-	echo "Building libzip for ${ANDROID_TARGET_PLATFORM}" 
-	
+#--------------------------------------------------------------------
+build_it()
+{
+    # builds either a static or shared lib depending on parm passed (ON or OFF)
+    want_shared=$1
 
-
-	
-	#run configuration for target platform
 	cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake \
 		-DCMAKE_INSTALL_PREFIX:PATH=../../${INSTALL_DIR}/${ANDROID_TARGET_PLATFORM} \
 		-DANDROID_ABI=${ANDROID_TARGET_PLATFORM} \
@@ -29,21 +27,29 @@ do
 		-DBUILD_TOOLS:BOOL=OFF \
 		-DBUILD_REGRESS:BOOL=OFF \
 		-DBUILD_EXAMPLES:BOOL=OFF \
-		-DBUILD_SHARED_LIBS:BOOL=ON \
+		-DBUILD_SHARED_LIBS:BOOL=$want_shared \
 		-DBUILD_DOC:BOOL=OFF \
 		-DANDROID_TOOLCHAIN=clang  cmake -H.. -B$BUILD_DIR/${ANDROID_TARGET_PLATFORM}
+		   	
+        #run make with all system threads and install
+        cd $BUILD_DIR/${ANDROID_TARGET_PLATFORM}
+        make install -j$(nproc --all)
+        cd $START_DIR
+    }
+
+#--------------------------------------------------------------------
+for ANDROID_TARGET_PLATFORM in armeabi-v7a arm64-v8a x86 x86_64
+do
+	echo "Building libzip for ${ANDROID_TARGET_PLATFORM}" 
+	
+	build_it ON
+	build_it OFF
 	
 	if [ $? -ne 0 ]; then
 		echo "Error executing: cmake"
 		exit 1
 	fi
-	
-	#run make with all system threads and install to output dir
-	cd $BUILD_DIR/${ANDROID_TARGET_PLATFORM}
-	make install -j$(nproc --all)
-	
-	
-	cd $START_DIR
+
 	
 	if [ $? -ne 0 ]; then
 		echo "Error executing make install for platform: ${ANDROID_TARGET_PLATFORM}"
