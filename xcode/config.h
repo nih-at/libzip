bbb09e2d8074e050f65c20d6a94983eabe70f4e4
Dieter Baron <dillo@nih.at>
2019-11-20 20:17:58 +0100

    Remove defunct iOS targets, update.

--- xcode/config.h
+++ xcode/config.h
@@ -1,225 +1,95 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_12
-#define HAVE_CLONEFILE 1
+#ifndef HAD_CONFIG_H
+#define HAD_CONFIG_H
+#ifndef _HAD_ZIPCONF_H
+#include "zipconf.h"
 #endif
-
-/* Define to 1 if you have the declaration of `tzname', and to 0 if you don't.
- */
-/* #undef HAVE_DECL_TZNAME */
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
-
-/* Define to 1 if you have the `fseeko' function. */
-#define HAVE_FSEEKO 1
-
-/* Define to 1 if you have the `ftello' function. */
-#define HAVE_FTELLO 1
-
-/* Define to 1 if you have the <fts.h> header file. */
-#define HAVE_FTS_H 1
-
-/* Define to 1 if you have the `getopt' function. */
-#define HAVE_GETOPT 1
-
-/* Define to 1 if the system has the type `int16_t'. */
-#define HAVE_INT16_T 1
-
-/* Define to 1 if the system has the type `int32_t'. */
-#define HAVE_INT32_T 1
-
-/* Define to 1 if the system has the type `int64_t'. */
-#define HAVE_INT64_T 1
-
-/* Define to 1 if the system has the type `int8_t'. */
-#define HAVE_INT8_T 1
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the `z' library (-lz). */
-#define HAVE_LIBZ 1
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the `mkstemp' function. */
-#define HAVE_MKSTEMP 1
-
-/* Define to 1 if you have the `open' function. */
-#define HAVE_OPEN 1
-
-/* Define to 1 if you have the `snprintf' function. */
-#define HAVE_SNPRINTF 1
-
-/* Define to 1 if the system has the type `ssize_t'. */
-#define HAVE_SSIZE_T 1
-
-/* Define to 1 if you have the <stdbool.h> header file. */
-#define HAVE_STDBOOL_H 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the `strcasecmp' function. */
-#define HAVE_STRCASECMP 1
-
-/* Define to 1 if you have the `strdup' function. */
-#define HAVE_STRDUP 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if `tm_zone' is a member of `struct tm'. */
-#define HAVE_STRUCT_TM_TM_ZONE 1
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if your `struct tm' has `tm_zone'. Deprecated, use
-   `HAVE_STRUCT_TM_TM_ZONE' instead. */
-#define HAVE_TM_ZONE 1
-
-/* Define to 1 if you don't have `tm_zone' but do have the external array
-   `tzname'. */
-/* #undef HAVE_TZNAME */
-
-/* Define to 1 if the system has the type `uint16_t'. */
-#define HAVE_UINT16_T 1
-
-/* Define to 1 if the system has the type `uint32_t'. */
-#define HAVE_UINT32_T 1
-
-/* Define to 1 if the system has the type `uint64_t'. */
-#define HAVE_UINT64_T 1
-
-/* Define to 1 if the system has the type `uint8_t'. */
-#define HAVE_UINT8_T 1
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to 1 or 0, depending whether the compiler supports simple visibility
-   declarations. */
-#define HAVE_VISIBILITY 1
-
-/* Define to 1 if you have the `_close' function. */
+/* BEGIN DEFINES */
+/* #undef HAVE___PROGNAME */
+/* #undef HAVE__CHMOD */
 /* #undef HAVE__CLOSE */
-
-/* Define to 1 if you have the `_dup' function. */
 /* #undef HAVE__DUP */
-
-/* Define to 1 if you have the `_fdopen' function. */
 /* #undef HAVE__FDOPEN */
-
-/* Define to 1 if you have the `_fileno' function. */
 /* #undef HAVE__FILENO */
-
-/* Define to 1 if you have the `_open' function. */
 /* #undef HAVE__OPEN */
-
-/* Define to 1 if you have the `_snprintf' function. */
+/* #undef HAVE__SETMODE */
 /* #undef HAVE__SNPRINTF */
-
-/* Define to 1 if you have the `_strdup' function. */
 /* #undef HAVE__STRDUP */
-
-/* Define to 1 if you have the `_stricmp' function. */
 /* #undef HAVE__STRICMP */
-
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
- */
-#define LT_OBJDIR ".libs/"
-
-/* Name of package */
-#define PACKAGE "libzip"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "libzip@nih.at"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "libzip"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libzip 1.0.1a"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libzip"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "1.3.1a"
-
-/* The size of `int', as computed by sizeof. */
-#define SIZEOF_INT 4
-
-/* The size of `long', as computed by sizeof. */
-#ifndef __LP64__
-#define SIZEOF_LONG 4
-#else /* __LP64__ */
-#define SIZEOF_LONG 8
-#endif /* __LP64__ */
-
-/* The size of `long long', as computed by sizeof. */
-#define SIZEOF_LONG_LONG 8
-
-/* The size of `off_t', as computed by sizeof. */
-#define SIZEOF_OFF_T 8
-
-/* The size of `short', as computed by sizeof. */
-#define SIZEOF_SHORT 2
-
-/* The size of `size_t', as computed by sizeof. */
-#ifndef __LP64__
-#define SIZEOF_SIZE_T 4
-#else /* __LP64__ */
-#define SIZEOF_SIZE_T 8
-#endif /* __LP64__ */
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Define to 1 if your <sys/time.h> declares `struct tm'. */
-/* #undef TM_IN_SYS_TIME */
-
-/* Version number of package */
-#define VERSION "1.3.1a"
-
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-#define _DARWIN_USE_64_BIT_INODE 1
-#endif
-
-/* Number of bits in a file offset, on hosts where this is settable. */
-/* #undef _FILE_OFFSET_BITS */
-
-/* Define for large files, on AIX-style hosts. */
-/* #undef _LARGE_FILES */
-
-
-#ifndef HAVE_SSIZE_T
-#if SIZEOF_SIZE_T == SIZEOF_INT
-typedef int ssize_t;
-#elif SIZEOF_SIZE_T == SIZEOF_LONG
-typedef long ssize_t;
-#elif SIZEOF_SIZE_T == SIZEOF_LONG_LONG
-typedef long long ssize_t;
-#else
-#error no suitable type for ssize_t found
-#endif
-#endif
-
+/* #undef HAVE__STRTOI64 */
+/* #undef HAVE__STRTOUI64 */
+/* #undef HAVE__UMASK */
+/* #undef HAVE__UNLINK */
+#define HAVE_ARC4RANDOM
+#define HAVE_CLONEFILE
 #define HAVE_COMMONCRYPTO
+#define HAVE_CRYPTO
+/* #undef HAVE_FICLONERANGE */
+#define HAVE_FILENO
+#define HAVE_FSEEKO
+#define HAVE_FTELLO
+#define HAVE_GETPROGNAME
+/* #undef HAVE_GNUTLS */
+#define HAVE_LIBBZ2
+#define HAVE_LIBLZMA
+#define HAVE_LOCALTIME_R
+/* #undef HAVE_MBEDTLS */
+/* #undef HAVE_MKSTEMP */
+#define HAVE_NULLABLE
+#define HAVE_OPEN
+/* #undef HAVE_OPENSSL */
+#define HAVE_SETMODE
+#define HAVE_SNPRINTF
+#define HAVE_SSIZE_T_LIBZIP
+#define HAVE_STRCASECMP
+#define HAVE_STRDUP
+/* #undef HAVE_STRICMP */
+#define HAVE_STRTOLL
+#define HAVE_STRTOULL
+/* #undef HAVE_STRUCT_TM_TM_ZONE */
+#define HAVE_STDBOOL_H
+#define HAVE_STRINGS_H
+#define HAVE_UNISTD_H
+/* #undef HAVE_WINDOWS_CRYPTO */
+/* #undef __INT8_LIBZIP */
+#define INT8_T_LIBZIP 1
+#define UINT8_T_LIBZIP 1
+/* #undef __INT16_LIBZIP */
+#define INT16_T_LIBZIP 2
+#define UINT16_T_LIBZIP 2
+/* #undef __INT32_LIBZIP */
+#define INT32_T_LIBZIP 4
+#define UINT32_T_LIBZIP 4
+/* #undef __INT64_LIBZIP */
+#define INT64_T_LIBZIP 8
+#define UINT64_T_LIBZIP 8
+#define SHORT_LIBZIP 2
+#define INT_LIBZIP 4
+#define LONG_LIBZIP 8
+#define LONG_LONG_LIBZIP 8
+#define SIZEOF_OFF_T 8
+#define SIZE_T_LIBZIP 8
+#define SSIZE_T_LIBZIP 8
+/* #undef HAVE_DIRENT_H */
+#define HAVE_FTS_H
+/* #undef HAVE_NDIR_H */
+/* #undef HAVE_SYS_DIR_H */
+/* #undef HAVE_SYS_NDIR_H */
+/* #undef WORDS_BIGENDIAN */
+#define HAVE_SHARED
+/* END DEFINES */
+#define PACKAGE "libzip"
+#define VERSION "1.5.2a"
+
+#ifndef HAVE_SSIZE_T_LIBZIP
+#  if SIZE_T_LIBZIP == INT_LIBZIP
+typedef int ssize_t;
+#  elif SIZE_T_LIBZIP == LONG_LIBZIP
+typedef long ssize_t;
+#  elif SIZE_T_LIBZIP == LONG_LONG_LIBZIP
+typedef long long ssize_t;
+#  else
+#error no suitable type for ssize_t found
+#  endif
+#endif
+
+#endif /* HAD_CONFIG_H */
