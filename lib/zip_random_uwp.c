2bba8fcea39c446d77e4671d550931417d9a4fbf
Thomas Klausner <tk@giga.or.at>
2020-03-08 21:27:05 +0100

    Use random functions source files on Windows.
    Use srand/rand instead of srandom/random (from Michał Janiszewski)
    
    Closes #156.

--- lib/zip_random_uwp.c
+++ lib/zip_random_uwp.c
@@ -68,17 +68,17 @@ zip_secure_random(zip_uint8_t *buffer, zip_uint16_t length) {
 zip_uint32_t
 zip_random_uint32(void) {
     static bool seeded = false;
-    
+
     zip_uint32_t value;
-    
+
     if (zip_secure_random((zip_uint8_t *)&value, sizeof(value))) {
         return value;
     }
-    
+
     if (!seeded) {
-        srandom((unsigned int)time(NULL));
+        srand((unsigned int)time(NULL));
     }
-    
-    return (zip_uint32_t)random();
+
+    return (zip_uint32_t)rand();
 }
 #endif
