4457e9197d4cc2cd7179e550b68842b91defebb2
Thomas Klausner <wiz@gatalith.at>
2022-06-13 17:04:36 +0200

    Bump copyright dates.

--- regress/liboverride-test.c
+++ regress/liboverride-test.c
@@ -1,7 +1,7 @@
 /*
   liboverride.c -- override function called by zip_open()
 
-  Copyright (C) 2017-2021 Dieter Baron and Thomas Klausner
+  Copyright (C) 2017-2022 Dieter Baron and Thomas Klausner
 
   This file is part of ckmame, a program to check rom sets for MAME.
   The authors can be contacted at <ckmame@nih.at>
