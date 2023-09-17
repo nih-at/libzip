e4d0533d44228fc1b3d5419bd777525f8a9c16b4
Thomas Klausner <wiz@gatalith.at>
2023-09-17 20:28:37 +0200

    clang-format, fix a lifetime issue, add more error handling

--- regress/fuzzers/zip_read_file_fuzzer.cc
+++ regress/fuzzers/zip_read_file_fuzzer.cc
@@ -1,73 +1,89 @@
-#include <zip.h>
-#include <zipint.h>
-#include <iostream>
 #include <fstream>
-#include <string>  
+#include <iostream>
+#include <string>
+#include <zip.h>
 
 std::string random_string(size_t length);
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
+#if 0
+    /* fix autoindent */
+    }
+#endif
 #endif
 
 /**
-This fuzzing target takes input data, creates a ZIP archive from it, checks the archive's consistency, 
-and iterates over the entries in the archive, reading data from each entry.
+   This fuzzing target takes input data, creates a ZIP archive from it, checks the archive's consistency,
+   and iterates over the entries in the archive, reading data from each entry.
 **/
 
-int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{
-    zip_source_t* src;
-    zip_t* za;
+int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    zip_source_t *src;
+    zip_t *za;
     zip_error_t error;
     char buf[32768];
     zip_int64_t i, n;
-    zip_file_t* f;
+    zip_file_t *f;
 
     std::string name = random_string(20) + ".zip";
 
     std::ofstream outfile(name, std::ios::binary);
 
     if (outfile.is_open()) {
-        outfile.write(reinterpret_cast<const char*>(data), size);
+        outfile.write(reinterpret_cast<const char *>(data), size);
         outfile.close();
 
         za = zip_open(name.c_str(), 0, NULL);
+        if (za == NULL) {
+            std::cerr << "Error opening archive." << std::endl;
+            return 1;
+        }
+
         n = zip_get_num_entries(za, 0);
         for (i = 0; i < n; i++) {
             f = zip_fopen_index(za, i, 0);
-            if (f == NULL) {                    
-                continue;
+            if (f == NULL) {
+                std::cerr << "Unable to open file." << std::endl;
+                zip_close(za);
+                return 1;
             }
 
             while (zip_fread(f, buf, sizeof(buf)) > 0) {
                 ;
-            }       
-            zip_fclose(f);
-                     
+            }
+            if (zip_fclose(f) < 0) {
+                std::cerr << "Error closing file." << std::endl;
+                zip_close(za);
+                return 1;
+            }
         }
-        zip_close(za);
-        std::remove(name.c_str()); 
-
+        if (zip_close(za) < 0) {
+            std::cerr << "Error closing archive." << std::endl;
+            return 1;
+        }
+        std::remove(name.c_str());
     }
     else {
         std::cerr << "Unable to open the file." << std::endl;
+        return 1;
     }
-        
+
     return 0;
 }
 
-std::string random_string(size_t length)
-{
+std::string
+random_string(size_t length) {
     auto randchar = []() -> char {
-        const char charset[] =
-            "0123456789"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz";
+        const char charset[] = "0123456789"
+                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                               "abcdefghijklmnopqrstuvwxyz";
         const size_t max_index = (sizeof(charset) - 1);
         return charset[rand() % max_index];
     };
     std::string str(length, 0);
     std::generate_n(str.begin(), length, randchar);
     return str;
+}
 }
--- regress/fuzzers/zip_write_encrypt_pkware_file_fuzzer.cc
+++ regress/fuzzers/zip_write_encrypt_pkware_file_fuzzer.cc
@@ -1,69 +1,83 @@
-#include <zip.h>
-#include <zipint.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <zip.h>
 
 std::string random_string(size_t length);
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
+#if 0
+    /* fix autoindent */
+    }
+#endif
 #endif
 
 /**
-This fuzzing target takes input data, creates a ZIP archive, load it to a buffer, adds a file to it 
-with traditional PKWARE encryption and a specified password, and then closes and removes the archive. 
+This fuzzing target takes input data, creates a ZIP archive, load
+it to a buffer, adds a file to it with traditional PKWARE
+encryption and a specified password, and then closes and removes
+the archive.
 
-The purpose of this fuzzer is to test security of ZIP archive handling and encryption in the libzip 
-by subjecting it to various inputs, including potentially malicious or malformed data of different file types.
+The purpose of this fuzzer is to test security of ZIP archive
+handling and encryption in the libzip by subjecting it to various
+inputs, including potentially malicious or malformed data of
+different file types.
 **/
 
-int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{
-    zip_source_t* src;
-    zip_t* za;
+int
+LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    zip_source_t *src;
+    zip_t *za;
     char buf[32768];
     zip_int64_t i, n;
-    zip_file_t* f;
+    zip_file_t *f;
 
-    std::string path = random_string(20) + "_pkware"+ ".zip";
-    const char *password = random_string(20).c_str();
-    const char * file    = random_string(20).c_str();
+    std::string path = random_string(20) + "_pkware" + ".zip";
+    std::string password = random_string(20);
+    std::string file = random_string(20);
     int error = 0;
     struct zip *archive = zip_open(path.c_str(), ZIP_CREATE, &error);
 
-    if(error)
+    if (error) {
         return -1;
+    }
 
     struct zip_source *source = zip_source_buffer(archive, data, size, 0);
-    if(source == NULL){
-        printf("failed to create source buffer. %s\n",zip_strerror(archive));
+    if (source == NULL) {
+        printf("failed to create source buffer. %s\n", zip_strerror(archive));
         return -1;
     }
 
-    int index = (int)zip_file_add(archive, file, source, ZIP_FL_OVERWRITE);
-    if(index < 0){
-        printf("failed to add file to archive: %s\n",zip_strerror(archive));
+    int index = (int)zip_file_add(archive, file.c_str(), source, ZIP_FL_OVERWRITE);
+    if (index < 0) {
+        printf("failed to add file to archive: %s\n", zip_strerror(archive));
         return -1;
     }
-    zip_file_set_encryption(archive, index, ZIP_EM_TRAD_PKWARE,/* Password to encrypt file */ password);
-    zip_close(archive);
-    std::remove(path.c_str()); 
+    if (zip_file_set_encryption(archive, index, ZIP_EM_TRAD_PKWARE, password.c_str()) < 0) {
+        printf("failed to set file encryption: %s\n", zip_strerror(archive));
+        return -1;
+    }
+    if (zip_close(archive) < 0) {
+        printf("error closing archive: %s\n", zip_strerror(archive));
+        return -1;
+    }
+    std::remove(path.c_str());
 
     return 0;
 }
 
-std::string random_string(size_t length)
-{
+std::string
+random_string(size_t length) {
     auto randchar = []() -> char {
-        const char charset[] =
-            "0123456789"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz";
+        const char charset[] = "0123456789"
+                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                               "abcdefghijklmnopqrstuvwxyz";
         const size_t max_index = (sizeof(charset) - 1);
         return charset[rand() % max_index];
     };
     std::string str(length, 0);
     std::generate_n(str.begin(), length, randchar);
     return str;
+}
 }
