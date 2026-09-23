5aeb57b6e922934125008756998b3ce2a93ba498
ae <ae@ae.tailb934bf.ts.net>
2026-09-23 13:01:36 +0200

    Select an installed ARM SDK for AppVeyor dependency builds

---
+++ .github/appveyor-triplets/arm-windows.cmake
@@ -0,0 +1,5 @@
+set(VCPKG_TARGET_ARCHITECTURE arm)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${VCPKG_ROOT_DIR}/scripts/toolchains/windows.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/arm-sdk.cmake")
